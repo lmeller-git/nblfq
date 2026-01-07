70606d67e5e84904cdc06b930147a9e221bc6d15
lmeller-git <louis.meller@icloud.com>
2026-01-07 14:20:40 +0100

    add more funcs to profiling

--- profiling/src/main.rs
+++ profiling/src/main.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code, unused_imports)]
+
 use std::{
     hint::{black_box, spin_loop},
     sync::atomic::{AtomicU64, Ordering},
@@ -9,15 +11,66 @@ use nblf_queue::{MPMCQueue, PooledQueue, Queue};
 const TOTAL_ITEMS: u64 = 30_000;
 const THREADS: u64 = 3;
 const ITEMS_PER_THREAD: u64 = TOTAL_ITEMS / THREADS;
+const CAP: usize = 64;
 
 fn main() {
-    let q = PooledQueue::new(64);
-    run_mpmc(q);
+    let q = Queue::new(CAP);
+    run_spsc(q);
 }
 
-// fn run_spsc() {}
+fn run_spsc<Q>(q: Q)
+where
+    Q: MPMCQueue<Item = &'static i32> + Sync,
+{
+    static ONE: i32 = 1;
+    assert_eq!(TOTAL_ITEMS % THREADS, 0);
 
-// fn run_mpsc() {}
+    thread::scope(|scope| {
+        scope.spawn(|| {
+            for _ in 0..TOTAL_ITEMS {
+                while q.push(black_box(&ONE)).is_err() {
+                    spin_loop();
+                }
+            }
+        });
+
+        scope.spawn(|| {
+            for _ in 0..TOTAL_ITEMS {
+                while black_box(q.pop()).is_none() {
+                    spin_loop();
+                }
+            }
+        });
+    })
+}
+
+fn run_mpsc<Q>(q: Q)
+where
+    Q: MPMCQueue<Item = &'static i32> + Sync,
+{
+    static ONE: i32 = 1;
+    assert_eq!(TOTAL_ITEMS % THREADS, 0);
+
+    thread::scope(|scope| {
+        for _ in 0..THREADS {
+            scope.spawn(|| {
+                for _ in 0..ITEMS_PER_THREAD {
+                    while q.push(black_box(&ONE)).is_err() {
+                        spin_loop();
+                    }
+                }
+            });
+        }
+
+        scope.spawn(|| {
+            for _ in 0..TOTAL_ITEMS {
+                while black_box(q.pop()).is_none() {
+                    spin_loop();
+                }
+            }
+        });
+    })
+}
 
 fn run_mpmc<Q>(q: Q)
 where
@@ -49,6 +102,7 @@ where
                         black_box(item);
                         counter.fetch_sub(1, Ordering::Release);
                     }
+                    spin_loop();
                 }
             });
         }
