5a5eac6dac3efda497bcb1f92e577789fcb58db0
lmeller-git <louis.meller@icloud.com>
2026-06-21 14:03:12 +0200

    clean up

--- src/sync.rs
+++ src/sync.rs
@@ -23,10 +23,6 @@ mod shuttle_ {
                 UnsafeCell(core::cell::UnsafeCell::new(data))
             }
 
-            pub(crate) fn with<R>(&self, f: impl FnOnce(*const T) -> R) -> R {
-                f(self.0.get())
-            }
-
             pub(crate) fn with_mut<R>(&self, f: impl FnOnce(*mut T) -> R) -> R {
                 f(self.0.get())
             }
@@ -60,10 +56,6 @@ mod core_ {
         impl<T> UnsafeCell<T> {
             pub(crate) fn new(data: T) -> UnsafeCell<T> {
                 UnsafeCell(core::cell::UnsafeCell::new(data))
-            }
-
-            pub(crate) fn with<R>(&self, f: impl FnOnce(*const T) -> R) -> R {
-                f(self.0.get())
             }
 
             pub(crate) fn with_mut<R>(&self, f: impl FnOnce(*mut T) -> R) -> R {
